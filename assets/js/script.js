9ff360181b4ef83ea5a33772b0f0d51b54e9627f
Jay <j.thomastt1@gmail.com>
2022-10-31 21:12:13 -0600

    done css /html

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,3 +3,4 @@ var city;
 api.openweathermap.org/data/2.5/weather?q={city}&appid={APIKey}
 var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
 fetch(queryURL) 
+
