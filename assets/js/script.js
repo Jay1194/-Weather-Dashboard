7a8b6095964eea7c26d5df4d56389375d7507a1b
Jay <j.thomastt1@gmail.com>
2022-11-01 01:01:47 -0600

    created variables / added to html

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,14 @@
-var APIKey = "0e2c5ea3894d1551db528791aa63e1aa";
+const APIKey = "0e2c5ea3894d1551db528791aa63e1aa";
 var city;
 api.openweathermap.org/data/2.5/weather?q={city}&appid={APIKey}
-var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
+const queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
 fetch(queryURL) 
+
+var temp = document.getElementById('temp');
+var hum = document.getElementById('hum');
+var windS = document.getElementById('wind');
+var clearB = document.getElementById('clear');
+var searchC = document.getElementById('sCity');
+var searchB = document.getElementById('sbutt');
+var city="";
 
